efa091f6f749be2355d47a14858955c6a01451b7
Leon-Leyang <leyang_hu@brown.edu>
2024-01-26 16:49:54 -0500

    Complete the main script

--- main.py
+++ main.py
@@ -1,8 +1,103 @@
+import os
+import torch
 from util.experiment import set_seeds, get_args
+from torch.utils.tensorboard import SummaryWriter
+from model.videoencoder import VideoEncoder
+from model.seegencoder import SEEGEncoder
+from train.train import train
+from eval.eval import eval
 
 
 def main(args):
-    pass
+    # Set up the experiment folder
+    exp_folder = os.path.join('./experiments', args.exp_name)
+    ckpt_folder = os.path.join(exp_folder, 'ckpt')
+    log_folder = os.path.join(exp_folder, 'log')
+    os.makedirs(exp_folder, exist_ok=True)
+    os.makedirs(ckpt_folder, exist_ok=True)
+    os.makedirs(log_folder, exist_ok=True)
+
+    # Set up the logger.
+    writer = SummaryWriter(log_dir=log_folder)
+
+    # Set up the device.
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Define the datasets and dataloaders
+    # TODO: Define the datasets and dataloaders.
+    print('Loading datasets and dataloaders ...')
+    train_dataset = None
+    val_dataset = None
+    test_dataset = None
+    train_loader = None
+    val_loader = None
+    test_loader = None
+
+    # Define the video encoder
+    print('Creating video encoder ...')
+    ckpt = "MCG-NJU/videomae-base"
+    video_encoder = VideoEncoder(ckpt)
+
+    # Define the seeg encoder
+    print('Creating sEEG encoder ...')
+    # TODO: Use the correct max_num_input_channels.
+    max_num_input_channels = 100
+    num_output_channels = args.num_output_channels
+    # TODO: Use the correct input_length
+    input_length = 2560
+    output_length = 1568
+    num_heads = args.num_heads
+    num_encoder_layers = args.num_encoder_layers
+    dim_feedforward = args.dim_feedforward
+    seeg_encoder = SEEGEncoder(max_num_input_channels, num_output_channels, input_length, output_length, num_heads,
+                               num_encoder_layers, dim_feedforward)
+
+    # Define the optimizer
+    optimizer = torch.optim.Adam(seeg_encoder.parameters(), lr=args.lr)
+
+    best_val_acc1 = 0.0
+    best_epoch = 0
+    start_epoch = 0
+
+    if args.ckpt:
+        print(f"Loading checkpoint from {args.ckpt}")
+        ckpt_state = torch.load(args.ckpt)
+        seeg_encoder.load_state_dict(ckpt_state['seeg_encoder'])
+        optimizer.load_state_dict(ckpt_state['optimizer'])
+        best_val_acc1 = ckpt_state['best_val_acc1']
+        best_epoch = ckpt_state['best_epoch']
+        start_epoch = ckpt_state['epoch']
+
+    for epoch in range(start_epoch, start_epoch + args.num_epochs):
+        # Training
+        train(epoch, video_encoder, seeg_encoder, optimizer, train_loader, writer, device)
+
+        # Validation
+        acc1, _ = eval(epoch, video_encoder, seeg_encoder, val_loader, writer, device, 'val')
+
+        if acc1 > best_val_acc1:
+            best_val_acc1 = acc1
+            best_epoch = epoch + 1
+
+        state = {
+            'seeg_encoder': seeg_encoder.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'best_val_acc1': best_val_acc1,
+            'best_epoch': best_epoch,
+            'epoch': epoch + 1,
+        }
+        ckpt_file = os.path.join(ckpt_folder, f'epoch_{epoch + 1}.pth')
+        torch.save(state, ckpt_file)
+
+    writer.close()
+
+    # Testing
+    if best_epoch <= start_epoch:
+        print('No best model found. Skip testing.')
+    else:
+        print(f'Testing the best model at epoch {best_epoch}...')
+        seeg_encoder.load_state_dict(torch.load(os.path.join(ckpt_folder, f'epoch_{best_epoch}.pth'))['seeg_encoder'])
+        eval(None, video_encoder, seeg_encoder, test_loader, writer, device, 'test')
 
 
 if __name__ == '__main__':
--- util/experiment.py
+++ util/experiment.py
@@ -22,13 +22,11 @@ def set_seeds(seed=42):
 
 def get_args():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--exp_name', '-e', type=str, default='lr_1e-3-batch_10-train_ratio-0.8',
+    arg_parser.add_argument('--exp_name', '-n', type=str, default='baseline',
                             help="The checkpoints and logs will be save in /experiments/$EXP_NAME")
     arg_parser.add_argument('--lr', '-l', type=float, default=1e-3, help="Learning rate")
-    arg_parser.add_argument('--save_freq', '-s', type=int, default=1, help="frequency of saving model")
-    arg_parser.add_argument('--total_epoch', '-t', type=int, default=20, help="total epoch number for training")
-    arg_parser.add_argument('--cont', '-c', action='store_true',
-                            help="whether to load saved the latest checkpoint from $EXP_NAME and continue training")
+    arg_parser.add_argument('--num_epochs', '-e', type=int, default=20, help="total number of epochs for training")
+    arg_parser.add_argument('--ckpt', '-c', type=str, default=None, help="path to the checkpoint file")
     arg_parser.add_argument('--batch_size', '-b', type=int, default=10, help="batch size")
     arg_parser.add_argument('--data_file', '-d', type=str, default='./data/data_segmented.npy',
                             help="path to the .npy file containing the data")
@@ -36,13 +34,13 @@ def get_args():
                             help="the ratio of training data to all data. 1/3 of the remaining data will be used for "
                                  "testing and 2/3 for validation")
     arg_parser.add_argument('--num_workers', '-w', type=int, default=4, help="Number of workers for dataloader")
-    arg_parser.add_argument('--num_output_channels', '-o', type=int, default=64,
+    arg_parser.add_argument('--num_output_channels', '-no', type=int, default=768,
                             help="number of output channels for the seeg encoder")
     arg_parser.add_argument('--num_heads', '-nh', type=int, default=2, help="Number of heads for the sEEG encoder")
-    arg_parser.add_argument('--num_encoder_layers', '-nl', type=int, default=6, help="number of encoder layers for the "
+    arg_parser.add_argument('--num_encoder_layers', '-ne', type=int, default=6, help="number of encoder layers for the "
                                                                                      "sEEG encoder")
-    arg_parser.add_argument('--dim_feedforward', '-f', type=int, default=2048, help="Hidden dimension for the seeg "
-                                                                                    "encoder")
+    arg_parser.add_argument('--dim_feedforward', '-df', type=int, default=2048, help="Hidden dimension for the seeg "
+                                                                                     "encoder")
     args = arg_parser.parse_args()
     return args
 

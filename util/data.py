d5d0db141b2d99606fe2ff22b6e0067f7699ec4b
Leon-Leyang <leyang_hu@brown.edu>
2024-01-28 18:48:20 -0500

    Refactor

--- util/data.py
+++ util/data.py
@@ -19,12 +19,11 @@ def get_frames(container, indices):
     """
     frames = []
     container.seek(0)
-    start_index = indices[0]
     end_index = indices[-1]
     for i, frame in enumerate(container.decode(video=0)):
         if i > end_index:
             break
-        if i >= start_index and i in indices:
+        if i in indices:
             frames.append(frame)
     return np.stack([x.to_ndarray(format="rgb24") for x in frames])
 

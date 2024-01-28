c6cfe6132d04024df15f25a17d237354787a2c99
Leon-Leyang <leyang_hu@brown.edu>
2024-01-28 16:47:29 -0500

    Refactor

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,29 +1,28 @@
+import os
 import numpy as np
 import pandas as pd
-import torch
-import torch.nn as
-import torch.nn.functional as F
 from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
 import cv2 as cv
 
-class MOVIESEEGDataset(Dataset):
-    def __init__(self, args):
 
-        self.ID = args.ID
-        self.phase = args.phase
-        self.seeg_path = "/oscar/data/brainstorm-ws/seeg_data/Memory Task Data/Epilepsy/Monitoring/"
-        self.video_path = "/oscar/data/brainstorm-ws/seeg_data/Movie Clips/"
-        df = pd.read_csv(self.seeg_path + self.ID + "_preprocessed_data.csv")
+class MovieSEEGDataset(Dataset):
+    def __init__(self, id, phase, seeg_dir, video_dir):
+        self.id = id
+        self.phase = phase
+        self.seeg_dir = seeg_dir
+        self.video_dir = video_dir
+
+        df = pd.read_csv(os.path.join(self.seeg_dir, self.id + "_preprocessed_data.csv"))
         df = df[df['Phase'] == self.phase]
         df = df.drop(['Error_Position', 'Error_Color'], axis=1)
         df = df.sort_values(['Condition', 'Electrode'])
+
         self.movie_clips = df['Condition'].unique()
         self.electrodes = df['Electrode'].unique()
         self.video_seeg = []
         for movie_clip in self.movie_clips:
             df_movie_clip = df[df['Condition'] == movie_clip].iloc[:, 4:]
-            cap = cv.VideoCapture(self.video_path + str(movie_clip) + ".avi")
+            cap = cv.VideoCapture(self.video_dir + str(movie_clip) + ".avi")
             frames = []
             while True:
                 ret, frame = cap.read()
@@ -40,3 +39,17 @@ class MOVIESEEGDataset(Dataset):
     def __getitem__(self, idx):
         movie_clip, df_movie_clip, frames = self.video_seeg[idx]
         return movie_clip, df_movie_clip, frames
+
+
+if __name__ == '__main__':
+    phase = 'Encoding'
+    id = 'e0010GP'
+    seeg_dir = '../data/dev/sEEG/'
+    video_dir = '../data/dev/Movie Clips/'
+
+    dataset = MovieSEEGDataset(id, phase, seeg_dir, video_dir)
+
+    for i in range(len(dataset)):
+        movie_clip, df_movie_clip, frames = dataset[i]
+        print(movie_clip, df_movie_clip, frames)
+

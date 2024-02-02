087792affaa813cf5d138591d72863c2c4ac8fc2
Yu-Ang Cheng(DELL) <68239011+Yu-AngCheng@users.noreply.github.com>
2024-02-02 15:47:14 -0500

    Revert "Update dataset4individual.py"
    This reverts commit 906b91c7d083ef9598dfa6259fa6b20ffaeeb195.

--- dataset/dataset4individual.py
+++ dataset/dataset4individual.py
@@ -3,15 +3,17 @@ import torch
 import pandas as pd
 from torch.utils.data import Dataset
 from util.data import process_video
-from torch.utils.data import random_split, DataLoader
-from torch.utils.data.sampler import BatchSampler
-import numpy as np
+
 
 class Dataset4Individual(Dataset):
-    def __init__(self, id, phases, seeg_dir):
+    def __init__(self, id, phases, seeg_dir, video_dir, video_processor_ckpt="MCG-NJU/videomae-base",
+                 num_frame_2_sample=16):
         self.id = id
         self.phases = phases
         self.seeg_dir = seeg_dir
+        self.video_dir = video_dir
+        self.video_processor_ckpt = video_processor_ckpt
+        self.num_frame_2_sample = num_frame_2_sample
 
         assert os.path.exists(os.path.join(self.seeg_dir, f'{self.id}_preprocessed_data.csv'))
         df = pd.read_csv(os.path.join(self.seeg_dir, f'{self.id}_preprocessed_data.csv'))
@@ -23,93 +25,42 @@ class Dataset4Individual(Dataset):
         if self.phases is None:
             self.phases = df['Phase'].unique()
 
-        self.data_by_condition = {idx: [] for idx in self.video_idxs}
+        self.data = []
         for video_idx in self.video_idxs:
+            assert os.path.exists(os.path.join(self.video_dir, f'mov{video_idx}.avi'))
+            video_path = os.path.join(self.video_dir, f'mov{video_idx}.avi')
+            # I would suggest we only load the video here
+            # and do the processing in __getitem__
+            video = process_video(video_path, self.video_processor_ckpt, self.num_frame_2_sample)
             for phase in self.phases:
                 seeg = df[(df['Condition'] == video_idx) & (df['Phase'] == phase)].iloc[:, 4:]
-                if not seeg.empty:
-                    self.data_by_condition[video_idx].append((seeg.values, video_idx - 1, self.phases.index(phase)))
-
+                seeg = torch.tensor(seeg.values, dtype=torch.float32)
+                self.data.append((seeg, video, torch.tensor(video_idx - 1, dtype=torch.float32),
+                                  self.phases.index(phase)))
 
     def __getitem__(self, idx):
-        condition, data_idx = idx
-        seeg, video_idx, phase = self.data_by_condition[condition][data_idx]
-        return seeg, video_idx, phase
+        seeg, video, video_idx, phase = self.data[idx]
+
+        # TODO: we need the downsample the seeg randomly
+        # TODO: we also need to select the random frame of the video and send it to the pretrained model
+
+        # TODO: Remove the following line. It is only for testing.
+        seeg_padding_mask = torch.zeros(seeg.shape[0], dtype=torch.bool)
+
+        return seeg, seeg_padding_mask, video, video_idx, phase
 
     def __len__(self):
-        return sum(len(items) for items in self.data_by_condition.values())
-
-
-class MovieSampler(BatchSampler):
-    def __init__(self, dataset, batch_size=30):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.conditions = list(dataset.data_by_condition.keys())
-
-    def __iter__(self):
-        batch = []
-        while len(batch) < self.batch_size:
-            for condition in np.random.permutation(self.conditions):
-                if len(self.dataset.data_by_condition[condition]) > 0:
-                    batch.append((condition, np.random.randint(len(self.dataset.data_by_condition[condition]))))
-                if len(batch) == self.batch_size:
-                    yield batch
-                    batch = []
-
-    def __len__(self):
-        return int(np.ceil(len(self.dataset) / self.batch_size))
-
-class MovieSampler2(BatchSampler):
-    def __init__(self, dataset, batch_size=30, drop_last=False):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.drop_last = drop_last
-        self.conditions = list(dataset.data_by_condition.keys())
-        # Initialize a structure to track sampled items for each condition
-        self.condition_indices = {condition: list(range(len(dataset.data_by_condition[condition]))) for condition in self.conditions}
-
-    def __iter__(self):
-        batch = []
-        while True:
-            np.random.shuffle(self.conditions)  # Shuffle conditions to ensure randomness
-            for condition in self.conditions:
-                if len(self.condition_indices[condition]) > 0:
-                    # Randomly select an index to sample from this condition
-                    index = np.random.choice(self.condition_indices[condition])
-                    batch.append((condition, index))
-                    # Remove the sampled index to avoid resampling
-                    self.condition_indices[condition].remove(index)
-                    if len(batch) == self.batch_size:
-                        yield batch
-                        batch = []
-            # Check if there are enough items left to form a new batch; if not, and drop_last is False, yield what's left
-            if not self.drop_last and len(batch) > 0 and all(len(indices) == 0 for indices in self.condition_indices.values()):
-                yield batch
-                break
-            # If all items have been sampled, stop the iterator
-            if all(len(indices) == 0 for indices in self.condition_indices.values()):
-                break
-
-    def __len__(self):
-        total_samples = sum(len(items) for items in self.dataset.data_by_condition.values())
-        if self.drop_last:
-            return total_samples // self.batch_size
-        else:
-            return (total_samples + self.batch_size - 1) // self.batch_size
-
-
+        return len(self.data)
 
 
 if __name__ == '__main__':
-    phase = ['Encoding', 'SameDayRecall', 'NextDayRecall']
+    phase = ['Encoding']
     id = 'e0010GP'
-    seeg_dir = '../data/dev/sEEG/'
+    seeg_dir = '../sEEG/'
+    video_dir = '../Movie Clips/'
 
-    dataset = Dataset4Individual(id, phase, seeg_dir)
-    sampler = MovieSampler(dataset, batch_size=30)
+    dataset = Dataset4Individual(id, phase, seeg_dir, video_dir)
 
-    # test the dataloader
-    data_loader = DataLoader(dataset, batch_sampler=sampler)
-
-    for i, (seeg, video_idx, phase) in enumerate(data_loader):
-        print(seeg.shape, video_idx, phase)
+    for i in range(len(dataset)):
+        seeg, seeg_padding_mask, video, video_idx, phase = dataset[i]
+        print(seeg.shape, seeg_padding_mask.shape, video.shape, video_idx, phase)

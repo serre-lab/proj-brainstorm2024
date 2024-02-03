3cb82c5cd647db85755f2fcffe5b2d5a14411f78
Leon-Leyang <leyang_hu@brown.edu>
2024-02-02 23:14:59 -0500

    Add `general_contrast_loss` and `agg_loss`

--- util/loss.py
+++ util/loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -13,3 +14,23 @@ def recon_loss(x, x_recon):
     - loss (torch.Tensor): A scalar tensor containing the reconstruction loss.
     """
     return nn.MSELoss()(x, x_recon)
+
+
+def general_contrast_loss(sim, label):
+    # TODO: Implement the general contrastive loss
+    return torch.tensor(0)
+
+
+def agg_loss(r_loss, c_loss, alpha=1.0):
+    """
+    Aggregate loss
+
+    Parameters:
+    - r_loss (torch.Tensor): A scalar tensor containing the reconstruction loss.
+    - c_loss (torch.Tensor): A scalar tensor containing the contrastive loss.
+    - alpha (float): A scalar weight for the contrastive loss.
+
+    Returns:
+    - loss (torch.Tensor): A scalar tensor containing the aggregated loss.
+    """
+    return r_loss + alpha * c_loss

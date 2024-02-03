63162edbef8a93f7034f48cc5c32b797a7a4bd32
Leon-Leyang <leyang_hu@brown.edu>
2024-02-02 21:37:36 -0500

    Refactor

--- util/model.py
+++ util/model.py
@@ -27,37 +27,5 @@ def gen_pos_encoding(seq_length, num_channels):
     return pe
 
 
-# class CustomMultiheadAttention(nn.Module):
-#     def __init__(self, dim, q_k_dim, v_dim, num_heads):
-#         super().__init__()
-#         self.q_k_dim = q_k_dim
-#         self.v_dim = v_dim
-#         self.num_heads = num_heads
-#
-#         self.qk_proj = nn.Linear(dim, q_k_dim * 2)
-#         self.v_proj = nn.Linear(dim, v_dim)
-#         self.out_proj = nn.Linear(v_dim, v_dim)
-#
-#         self.scale = torch.sqrt(torch.FloatTensor([q_k_dim // num_heads]))
-#
-#     def forward(self, x):
-#         B, N, C = x.shape
-#
-#         qk = self.qk_proj(x)
-#         qk = qk.reshape(B, N, 2, self.num_heads, -1).permute(2, 0, 3, 1, 4)
-#         q, k = qk[0], qk[1]
-#         v = self.v_proj(x).reshape(B, N, self.num_heads, -1).permute(0, 2, 1, 3)
-#
-#         attn = (q @ k.transpose(-2, -1)) / self.scale
-#         attn = attn.softmax(dim=-1)
-#
-#         out = (attn @ v).transpose(1, 2).reshape(B, N, -1)
-#         out = self.out_proj(out)
-#         return out
-
-
 if __name__ == '__main__':
-    x = torch.randn(2, 1000, 256)
-    attn = CustomMultiheadAttention(256, 64, 100, 4)
-    out = attn(x)
-    print(out.shape)
+    pass

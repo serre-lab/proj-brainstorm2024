df983bbc773d88b260156613b4dfe94b285ee518
Yu-Ang Cheng(DELL) <68239011+Yu-AngCheng@users.noreply.github.com>
2024-02-06 16:36:08 -0500

    new contrast loss

--- util/loss.py
+++ util/loss.py
@@ -16,9 +16,38 @@ def recon_loss(x, x_recon):
     return nn.MSELoss()(x, x_recon)
 
 
-def general_contrast_loss(sim, label):
-    # TODO: Implement the general contrastive loss
-    return torch.tensor(0)
+def general_contrast_loss(embeddings, movie_indices, temperature=0.07):
+
+    """
+    Calculate the contrastive loss using cos similarity for a batch of embeddings.
+    Args:
+        embeddings:
+        movie_indices:
+
+    Returns:
+
+    """
+
+    # Calculate cosine similarity matrix
+    embeddings = F.normalize(embeddings, p=2, dim=1)
+    sim = torch.matmul(embeddings, embeddings.T) / temperature
+    sim_softmax = F.softmax(sim, dim=1)
+
+    # Mask for positive and negative pairs
+    positive_mask = movie_indices.unsqueeze(1) == movie_indices.unsqueeze(0)
+    negative_mask = ~positive_mask
+    # Mask for excluding self-similarity
+    positive_mask.masked_fill_(torch.eye(embeddings.size(0), device=embeddings.device).bool(),
+                               torch.tensor(0.0, device=embeddings.device))
+
+    # Calculate the sim_softmax for negative pairs
+    sim_softmax_negative = sim_softmax * negative_mask.float()
+    sim_softmax_negative = sim_softmax_negative.sum(dim=1)/negative_mask.float().sum(dim=1)
+
+    # loss is mean of the negative sim_softmax
+    c_loss = torch.log(sim_softmax_negative).mean()
+
+    return c_loss
 
 
 def contrastive_loss(embeddings, movie_indices, margin=1.0):

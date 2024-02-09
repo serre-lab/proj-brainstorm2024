013af51e170dbda6572e2bbcce417280cda5e8b9
Yu-Ang Cheng(DELL) <68239011+Yu-AngCheng@users.noreply.github.com>
2024-02-08 22:30:36 -0500

    Update classifier.py

--- model/classifier.py
+++ model/classifier.py
@@ -4,17 +4,13 @@ import torch.nn as nn
 class LinearClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear1 = nn.Linear(128 * 14 * 20, 1024)
-        self.linear3 = nn.Linear(1024, 256)
-        self.linear5 = nn.Linear(256, 30)
+        self.linear1 = nn.Linear(128 * 14 * 20, 30)
 
 
     def forward(self, x):
         batch_size = x.size(0)
         x = x.view(batch_size, -1)
-        x = nn.functional.relu(self.linear1(x))
-        x = nn.functional.relu(self.linear3(x))
-        return self.linear5(x)
+        return self.linear1(x)
 
 
 if __name__ == '__main__':

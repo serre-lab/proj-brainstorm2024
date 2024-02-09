363f8c1523e44cb3cf6f22e74274284b8999bc37
Leon-Leyang <leyang_hu@brown.edu>
2024-02-08 19:04:25 -0500

    Clean up args

--- util/experiment.py
+++ util/experiment.py
@@ -55,12 +55,12 @@ def get_args():
     arg_parser.add_argument('--classifier_epochs', '-ce', type=int, default=100, help="Classifier training epochs")
     arg_parser.add_argument('--ckpt', '-c', type=str, default=None, help="Path to the checkpoint file")
     arg_parser.add_argument('--batch_size', '-b', type=int, default=30, help="Batch size")
-    arg_parser.add_argument('--train_ratio', '-tr', type=float, default=0.7,
+    arg_parser.add_argument('--train_ratio', '-r', type=float, default=0.7,
                             help="The ratio of training data. The rest is validation data")
     arg_parser.add_argument('--num_workers', '-w', type=int, default=4, help="Number of workers for dataloader")
     arg_parser.add_argument('--alpha', '-a', type=float, default=1.0, help="Weight of the contrastive loss")
-    arg_parser.add_argument('--step_size', '-ss', type=int, default=30, help="Step size for the learning rate scheduler")
-    arg_parser.add_argument('--gamma', '-g', type=float, default=0.1, help="Gamma for the learning rate scheduler")
+    arg_parser.add_argument('--lr_step_size', '-ls', type=int, default=30, help="Step size for the learning rate scheduler")
+    arg_parser.add_argument('--lr_gamma', '-lg', type=float, default=0.1, help="Gamma for the learning rate scheduler")
     arg_parser.add_argument('--alpha_step_size', '-as', type=int, default=200, help="Step size for the alpha scheduler")
     arg_parser.add_argument('--alpha_gamma', '-ag', type=float, default=10, help="Gamma for the alpha scheduler")
     args = arg_parser.parse_args()

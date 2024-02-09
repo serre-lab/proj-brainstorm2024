54d501df42c10b751a0e7e60dad132c9735eb62a
Leon-Leyang <leyang_hu@brown.edu>
2024-02-08 19:02:18 -0500

    Clean up args

--- util/experiment.py
+++ util/experiment.py
@@ -49,25 +49,16 @@ def get_args():
     arg_parser.add_argument('--exp_name', '-n', type=str, default='baseline',
                             help="The checkpoints and logs will be save in /experiments/$EXP_NAME")
     arg_parser.add_argument('--seeg_dir', '-sd', type=str, default='/oscar/data/brainstorm-ws/seeg_data/Memory Task Data/Epilepsy/Monitoring', help="Root of the sEEG data")
-    arg_parser.add_argument('--video_dir', '-vd', type=str, default='./data/dev/Movie Clips/',  help="Root of the video"
-                                                                                                     " data")
     arg_parser.add_argument('--autoencoder_lr', '-al', type=float, default=1e-3, help="Autoencoder learning rate")
     arg_parser.add_argument('--classifier_lr', '-cl', type=float, default=1e-3, help="Classifier learning rate")
-    arg_parser.add_argument('--autoencoder_epochs', '-ae', type=int, default=1000, help="total number of epochs for autoencoder training")
-    arg_parser.add_argument('--classifier_epochs', '-ce', type=int, default=100, help="total number of epochs for classifier training")
-    arg_parser.add_argument('--ckpt', '-c', type=str, default=None, help="path to the checkpoint file")
-    arg_parser.add_argument('--batch_size', '-b', type=int, default=30, help="batch size")
+    arg_parser.add_argument('--autoencoder_epochs', '-ae', type=int, default=1000, help="Autoencoder training epochs")
+    arg_parser.add_argument('--classifier_epochs', '-ce', type=int, default=100, help="Classifier training epochs")
+    arg_parser.add_argument('--ckpt', '-c', type=str, default=None, help="Path to the checkpoint file")
+    arg_parser.add_argument('--batch_size', '-b', type=int, default=30, help="Batch size")
     arg_parser.add_argument('--train_ratio', '-tr', type=float, default=0.7,
-                            help="the ratio of training data to all data. The rest is validation data")
+                            help="The ratio of training data. The rest is validation data")
     arg_parser.add_argument('--num_workers', '-w', type=int, default=4, help="Number of workers for dataloader")
-    arg_parser.add_argument('--num_output_channels', '-no', type=int, default=768,
-                            help="number of output channels for the seeg encoder")
-    arg_parser.add_argument('--num_heads', '-nh', type=int, default=2, help="Number of heads for the sEEG encoder")
-    arg_parser.add_argument('--num_encoder_layers', '-ne', type=int, default=6, help="number of encoder layers for the "
-                                                                                     "sEEG encoder")
-    arg_parser.add_argument('--dim_feedforward', '-df', type=int, default=2048, help="Hidden dimension for the sEEG "
-                                                                                     "encoder")
-    arg_parser.add_argument('--alpha', '-a', type=float, default=1.0, help="Hyperparameter to control the weight of the contrastive loss")
+    arg_parser.add_argument('--alpha', '-a', type=float, default=1.0, help="Weight of the contrastive loss")
     arg_parser.add_argument('--step_size', '-ss', type=int, default=30, help="Step size for the learning rate scheduler")
     arg_parser.add_argument('--gamma', '-g', type=float, default=0.1, help="Gamma for the learning rate scheduler")
     arg_parser.add_argument('--alpha_step_size', '-as', type=int, default=200, help="Step size for the alpha scheduler")

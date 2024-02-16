c096bc93ef4631fbde11b041d96c1a75a37da6f1
Yu-Ang Cheng(DELL) <68239011+Yu-AngCheng@users.noreply.github.com>
2024-02-16 18:39:11 -0500

    update model
    delete dropout

--- main.py
+++ main.py
@@ -54,22 +54,22 @@ def main(args):
 
     print("number of parameters:", sum([p.numel() for p in e2e_classifier.parameters() if p.requires_grad==True]))
 
-    # Gradient Flow
-    def plot_grad_flow(named_parameters):
-        ave_grads = []
-        layers = []
-        for n, p in named_parameters:
-            if(p.requires_grad) and ("bias" not in n):
-                layers.append(n)
-                ave_grads.append(p.grad.abs().mean().cpu().numpy())
-        plt.plot(ave_grads, alpha=0.3, color="b")
-        plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
-        plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
-        plt.xlim(xmin=0, xmax=len(ave_grads))
-        plt.xlabel("Layers")
-        plt.ylabel("average gradient")
-        plt.title("Gradient flow")
-        plt.grid(True)
+    # # Gradient Flow
+    # def plot_grad_flow(named_parameters):
+    #     ave_grads = []
+    #     layers = []
+    #     for n, p in named_parameters:
+    #         if(p.requires_grad) and ("bias" not in n):
+    #             layers.append(n)
+    #             ave_grads.append(p.grad.abs().mean().cpu().numpy())
+    #     plt.plot(ave_grads, alpha=0.3, color="b")
+    #     plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    #     plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    #     plt.xlim(xmin=0, xmax=len(ave_grads))
+    #     plt.xlabel("Layers")
+    #     plt.ylabel("average gradient")
+    #     plt.title("Gradient flow")
+    #     plt.grid(True)
 
     # Define the optimizers
     optimizer = torch.optim.AdamW(e2e_classifier.parameters(), lr=args.lr, weight_decay=args.weight_decay)
--- model/autoencoder.py
+++ model/autoencoder.py
@@ -115,58 +115,58 @@ class ConvAutoEncoder(nn.Module):
             nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
             nn.BatchNorm1d(16),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=0.001),  # Add dropout here, adjust p as needed
             nn.Conv1d(in_channels=16, out_channels=16, kernel_size=3, padding=1),
             nn.BatchNorm1d(16),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=0.001),  # Add dropout here, adjust p as needed
             nn.MaxPool1d(kernel_size=2, stride=2),
 
             nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
             nn.BatchNorm1d(32),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
             nn.BatchNorm1d(32),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.MaxPool1d(kernel_size=2, stride=2),
 
             nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.BatchNorm1d(64),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
             nn.BatchNorm1d(64),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.MaxPool1d(kernel_size=2, stride=2),
 
             nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.BatchNorm1d(128),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.BatchNorm1d(128),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.MaxPool1d(kernel_size=2, stride=2),
 
             nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.BatchNorm1d(128),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
 
             nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.BatchNorm1d(128),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),  # Add dropout here, adjust p as needed,
+            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed,
 
             nn.MaxPool1d(kernel_size=2, stride=2),
         )
@@ -177,31 +177,31 @@ class ConvAutoEncoder(nn.Module):
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1),
             nn.BatchNorm2d(256), 
             nn.ReLU(True),
-            nn.Dropout2d(p=0.5),  # Adjust p as needed
+            # nn.Dropout2d(p=.001),  # Adjust p as needed
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
             nn.BatchNorm2d(256), 
             nn.ReLU(True),
-            nn.Dropout2d(p=0.5),  # Adjust p as needed
+            # nn.Dropout2d(p=.001),  # Adjust p as needed
             nn.MaxPool2d(kernel_size=(2, 2)),
 
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
             nn.BatchNorm2d(256), 
             nn.ReLU(True),
-            nn.Dropout2d(p=0.5),  # Adjust p as needed
+            # nn.Dropout2d(p=.001),  # Adjust p as needed
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
             nn.BatchNorm2d(256), 
             nn.ReLU(True),
-            nn.Dropout2d(p=0.5),  # Adjust p as needed
+            # nn.Dropout2d(p=.001),  # Adjust p as needed
             nn.MaxPool2d(kernel_size=(2, 2)),
 
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding=1),
             nn.BatchNorm2d(512), 
             nn.ReLU(True),
-            nn.Dropout2d(p=0.5),  # Adjust p as needed
+            # nn.Dropout2d(p=.001),  # Adjust p as needed
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1),
             nn.BatchNorm2d(512), 
             nn.ReLU(True),
-            nn.Dropout2d(p=0.5),  # Adjust p as needed
+            # nn.Dropout2d(p=.001),  # Adjust p as needed
             nn.MaxPool2d(kernel_size=(2, 2)),
         )
 

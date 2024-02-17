5bef5361e8d08d3289c7595f499d31d438aef6e4
Yu-Ang Cheng(office PC) <68239011+Yu-AngCheng@users.noreply.github.com>
2024-02-16 22:46:20 -0500

    update

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+import matplotlib
 
+matplotlib.use('Agg')
 
 class AutoEncoder(nn.Module):
     def __init__(self):
@@ -103,6 +106,95 @@ class AutoEncoder(nn.Module):
 #
 #         return x.squeeze(), embed
 
+class DownConv1d_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, p=0.001):
+        super(DownConv1d_block, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.bn2 = nn.BatchNorm1d(out_channels)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.dropout(x)
+        return x
+
+class DownConv2d_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, p=0.001):
+        super(DownConv2d_block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.dropout(x)
+        return x
+
+
+class UpConv1d_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, p=0.001):
+        super(UpConv1d_block, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.bn2 = nn.BatchNorm1d(out_channels)
+        self.relu = nn.ReLU()
+        self.upsample = nn.Upsample(scale_factor=2)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, x):
+        x = self.upsample(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        return x
+
+
+class UpConv2d_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, p=0.001):
+        super(UpConv2d_block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.upsample = nn.Upsample(scale_factor=2)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, x):
+        x = self.upsample(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        return x
 
 class ConvAutoEncoder(nn.Module):
     def __init__(self, num_electrodes):
@@ -111,194 +203,60 @@ class ConvAutoEncoder(nn.Module):
         self.fc2 = nn.ModuleList([nn.Linear(160, num_electrodes[i]) for i in range(len(num_electrodes))])
 
         # Initial 1D convolutions on the temporal dimension with MaxPooling
-        self.initial_conv1d = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm1d(16),
-            nn.ReLU(True),
-            # nn.Dropout(p=0.001),  # Add dropout here, adjust p as needed
-            nn.Conv1d(in_channels=16, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm1d(16),
-            nn.ReLU(True),
-            # nn.Dropout(p=0.001),  # Add dropout here, adjust p as needed
-            nn.MaxPool1d(kernel_size=2, stride=2),
+        self.Down_1D_1 = DownConv1d_block(1, 8)
+        self.Down_1D_2 = DownConv1d_block(8, 16)
+        self.Down_1D_3 = DownConv1d_block(16, 32)
+        self.Down_1D_4 = DownConv1d_block(32, 64)
+        self.Down_1D_5 = DownConv1d_block(64, 64)
 
-            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm1d(32),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
+        self.Down_2D_1 = DownConv2d_block(64, 128)
+        self.Down_2D_2 = DownConv2d_block(128, 256)
+        self.Down_2D_3 = DownConv2d_block(256, 512)
 
-            nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm1d(32),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
+        self.Up_2D_1 = UpConv2d_block(512, 256)  # Note the channel adjustments for concatenation
+        self.Up_2D_2 = UpConv2d_block(256 + 256, 128)
+        self.Up_2D_3 = UpConv2d_block(128 + 128, 64)  # Transition back to 1D happens after this
 
-            nn.MaxPool1d(kernel_size=2, stride=2),
-
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm1d(64),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
-
-            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm1d(64),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
-
-            nn.MaxPool1d(kernel_size=2, stride=2),
-
-            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm1d(128),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
-
-            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm1d(128),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
-
-            nn.MaxPool1d(kernel_size=2, stride=2),
-
-            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm1d(128),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed
-
-            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm1d(128),
-            nn.ReLU(True),
-            # nn.Dropout(p=.001),  # Add dropout here, adjust p as needed,
-
-            nn.MaxPool1d(kernel_size=2, stride=2),
-        )
-
-
-        # Encoder 2D convolutions for spatial-temporal features with MaxPooling
-        self.encoder_conv2d = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(256), 
-            nn.ReLU(True),
-            # nn.Dropout2d(p=.001),  # Adjust p as needed
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(256), 
-            nn.ReLU(True),
-            # nn.Dropout2d(p=.001),  # Adjust p as needed
-            nn.MaxPool2d(kernel_size=(2, 2)),
-
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(256), 
-            nn.ReLU(True),
-            # nn.Dropout2d(p=.001),  # Adjust p as needed
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(256), 
-            nn.ReLU(True),
-            # nn.Dropout2d(p=.001),  # Adjust p as needed
-            nn.MaxPool2d(kernel_size=(2, 2)),
-
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(512), 
-            nn.ReLU(True),
-            # nn.Dropout2d(p=.001),  # Adjust p as needed
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(512), 
-            nn.ReLU(True),
-            # nn.Dropout2d(p=.001),  # Adjust p as needed
-            nn.MaxPool2d(kernel_size=(2, 2)),
-        )
-
-
-        # Decoder 2D convolutions with Upsampling followed by Convolution
-        self.decoder_conv2d = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, padding=(1, 1)),
-            nn.BatchNorm2d(256), 
-            nn.ReLU(True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=(1, 1)),
-            nn.BatchNorm2d(256), 
-            nn.ReLU(True),
-
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=(1, 1)),
-            nn.BatchNorm2d(128), 
-            nn.ReLU(True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=(1, 1)),
-            nn.BatchNorm2d(128), 
-            nn.ReLU(True),
-
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=(1, 1)),
-            nn.BatchNorm2d(64), 
-            nn.ReLU(True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=(1, 1)),
-            nn.BatchNorm2d(64), 
-            nn.ReLU(True),
-        )
-
-        # Final 1D deconvolutions to restore original temporal dimension with Upsampling followed by Convolution
-        self.final_deconv1d = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv1d(in_channels=64, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm1d(32),
-            nn.ReLU(True),
-            nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm1d(32),
-            nn.ReLU(True),
-
-            nn.Upsample(scale_factor=2),
-            nn.Conv1d(in_channels=32, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm1d(16),
-            nn.ReLU(True),
-            nn.Conv1d(in_channels=16, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm1d(16),
-            nn.ReLU(True),
-
-            nn.Upsample(scale_factor=2),
-            nn.Conv1d(in_channels=16, out_channels=8, kernel_size=3, padding=1),
-            nn.BatchNorm1d(8),
-            nn.ReLU(True),
-            nn.Conv1d(in_channels=8, out_channels=8, kernel_size=3, padding=1),
-            nn.BatchNorm1d(8),
-            nn.ReLU(True),
-
-            nn.Upsample(scale_factor=2),
-            nn.Conv1d(in_channels=8, out_channels=4, kernel_size=3, padding=1),
-            nn.BatchNorm1d(4),
-            nn.ReLU(True),
-
-            nn.Conv1d(in_channels=4, out_channels=4, kernel_size=3, padding=1),
-            nn.BatchNorm1d(4),
-            nn.ReLU(True),
-
-            nn.Upsample(scale_factor=2),
-            nn.Conv1d(in_channels=4, out_channels=1, kernel_size=3, padding=1),
-            nn.BatchNorm1d(1),
-            nn.ReLU(True),
-            nn.Conv1d(in_channels=1, out_channels=1, kernel_size=3, padding=1),
-            nn.BatchNorm1d(1),
-            nn.ReLU(True),
-        )
-
+        self.Up_1D_1 = UpConv1d_block(64 + 64, 64)
+        self.Up_1D_2 = UpConv1d_block(64, 32)
+        self.Up_1D_3 = UpConv1d_block(32, 16)
+        self.Up_1D_4 = UpConv1d_block(16, 8)
+        self.Up_1D_5 = UpConv1d_block(8, 1)
 
     def forward(self, x, id):
         x = x.unsqueeze(2)  # Expecting shape to be (batch, 234, 1, 5120)
         batch_size, electrodes, channels, length = x.size()
 
         x = x.view(-1, channels, length)  # Flatten to (batch*234, 1, 5120)
-        x = self.initial_conv1d(x)  # Apply initial 1D convolutions with MaxPooling
+        x = self.Down_1D_1(x)
+        x = self.Down_1D_2(x)
+        x = self.Down_1D_3(x)
+        x = self.Down_1D_4(x)
+        x3 = self.Down_1D_5(x)
 
         # Reshape and permute for 2D convolutions
         x = x.view(batch_size, electrodes, -1, x.size(-1))
         x = x.permute(0, 2, 3, 1)
         x = self.fc1[id](x)
         x = x.permute(0, 1, 3, 2)
-        embed = self.encoder_conv2d(x)  # Apply 2D convolutions with MaxPooling
+        x4 = self.Down_2D_1(x)
+        x5 = self.Down_2D_2(x4)
+        embed = self.Down_2D_3(x5)  # Apply 2D convolutions with MaxPooling
 
-        x = self.decoder_conv2d(embed)
+        x = self.Up_2D_1(embed)
+        x = self.Up_2D_2(torch.cat([x, x5], dim=1))
+        x = self.Up_2D_3(torch.cat([x, x4], dim=1))
+
         x = x.permute(0, 1, 3, 2)
         x = self.fc2[id](x)
         x = x.permute(0, 3, 1, 2)  # Rearrange for decoding
         x = x.reshape(batch_size * electrodes, x.size(2), x.size(-1))  # Flatten for 1D deconvolutions
 
-        x = self.final_deconv1d(x)
+        x = self.Up_1D_1(torch.cat([x, x3], dim=1))
+        x = self.Up_1D_2(x)
+        x = self.Up_1D_3(x)
+        x = self.Up_1D_4(x)
+        x = self.Up_1D_5(x)
         x = x.view(batch_size, electrodes, channels, -1)
 
         return x.squeeze(), embed
@@ -311,6 +269,8 @@ if __name__ == '__main__':
 
     # model = AutoEncoder().to(device)
     model = ConvAutoEncoder(num_electrodes=[234, 233]).to(device)
+    # print number of parameters
+    print(f'Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}')
 
     input = torch.randn(5, 234, 5120).to(device)
 

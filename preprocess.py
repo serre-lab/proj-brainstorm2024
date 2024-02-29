5b141fa42dd9547082582d23915a24fddc721f4f
Yu-Ang Cheng(office PC) <68239011+Yu-AngCheng@users.noreply.github.com>
2024-02-28 22:40:46 -0500

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,15 @@
-import cv2
+import av
 import numpy as np
 from transformers import VideoMAEImageProcessor
+from transformers import AutoImageProcessor, AutoModel
 import torch
-
+from torchvision import transforms
+from PIL import Image
 
 def get_frames(file_path):
-    cap = cv2.VideoCapture(file_path)
-    frames = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # Convert the frame format from BGR to RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frames.append(frame_rgb)
-    cap.release()
-    return np.stack(frames)
+    container = av.open(file_path)
+    container.seek(0)
+    return np.stack([frame.to_ndarray(format="rgb24") for i, frame in enumerate(container.decode(video=0))])
 
 
 def videomae_preprocess(avi_path, ckpt="MCG-NJU/videomae-base"):
@@ -25,7 +19,7 @@ def videomae_preprocess(avi_path, ckpt="MCG-NJU/videomae-base"):
     for i in range(1, 20):
         file_path = avi_path + f'\greenbook_{i:02d}.avi'
         video = get_frames(file_path)
-        video_videomae = image_processor(np.array(video), return_tensors="pt")['pixel_values'][0]
+        video_videomae = image_processor(list(video), return_tensors="pt")['pixel_values'][0]
         video_data.append(video_videomae.numpy())
         print(f'greenbook_{i:02d}.avi: {video_videomae.shape}')
     video_data = np.array(video_data, dtype=np.float32)
@@ -53,8 +47,40 @@ def seeg_preprocess(seeg_path):
     np.save('seeg_split.npy', seeg_windows)
 
 
-def dinos_preprocess():
-    pass
+def dinos_preprocess(avi_path):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize the image
+        transforms.ToTensor(),  # Convert the image to a tensor and scale to [0, 1]
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
+    ])
+
+    frame_embeddings = []
+
+    model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14')
+    model.eval()
+
+    # load avi files from greenbook_01 to greenbook_19
+    for i in range(1, 20):
+        file_path = avi_path + f'\greenbook_{i:02d}.avi'
+        container = av.open(file_path)
+        container.seek(0)
+        for i, frame in enumerate(container.decode(video=0)):
+            frame_rgb = frame.to_ndarray(format="rgb24")
+            frame_preprocessed = transform(Image.fromarray(frame_rgb)).unsqueeze(0)
+            with torch.no_grad():
+                frame_embedding = model(frame_preprocessed)
+            frame_embeddings.append(frame_embedding)
+    frame_embeddings = torch.stack(frame_embeddings)
+    # split into 5s windows, drop the last window if it's less than 5s
+    video_sr = 30.0
+    window_size = int(5 * video_sr)
+    n_windows = int(frame_embeddings.shape[0] // window_size)
+    frame_embeddings = frame_embeddings[:n_windows * window_size]
+    frame_embeddings = frame_embeddings.split(window_size, dim=0)
+    frame_embeddings = torch.stack(frame_embeddings)
+    print(frame_embeddings.shape)
+    torch.save(frame_embeddings, 'greenbook_dino.pt')
+
 
 
 if __name__ == "__main__":
@@ -62,3 +88,4 @@ if __name__ == "__main__":
         r'C:\Users\ycheng70\OneDrive - Brown University\Documents\proj-subtitles-decoding\data\green_book')
     seeg_preprocess(
         r'C:\Users\ycheng70\OneDrive - Brown University\Documents\proj-subtitles-decoding\data\seeg_contacts.npy')
+    dinos_preprocess(r'C:\Users\ycheng70\OneDrive - Brown University\Documents\proj-subtitles-decoding\data\green_book')

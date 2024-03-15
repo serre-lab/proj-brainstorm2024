652532bb1a1471b75d8bd3a0a5fa61f44a1d4dbf
Leon-Leyang <leyang_hu@brown.edu>
2024-03-15 10:42:36 -0400

    Add scene version model

--- model/seegencoder.py
+++ model/seegencoder.py
@@ -104,6 +104,54 @@ class SEEGEncoderCls(nn.Module):
         return x
 
 
+class SEEGEncoderScene(nn.Module):
+    """
+    A Transformer Encoder for sEEG data. Scene based split.
+    Parameters:
+    - num_heads (int): The number of heads in the multi-head attention.
+    - num_encoder_layers (int): The number of encoder layers in the transformer.
+    - dim_feedforward (int): The dimension of the feedforward network in the transformer.
+    - num_input_channels (int): The number of input channels in the sEEG data.
+    - input_length (int): The length of the padded input sequence.
+    """
+    def __init__(self, num_heads=6, num_encoder_layers=6, dim_feedforward=2048, num_input_channels=84, input_length=5120):
+        super().__init__()
+        max_length = 6865
+
+        positional_encoding = gen_pos_encoding(max_length + 1, num_input_channels)
+        self.register_buffer('positional_encoding', positional_encoding)
+
+        # Transformer Encoder
+        encoder_layer = nn.TransformerEncoderLayer(d_model=num_input_channels, nhead=num_heads,
+                                                   dim_feedforward=dim_feedforward, batch_first=True)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, num_input_channels))
+
+        self.proj = nn.Linear(num_input_channels, 768)
+
+    def forward(self, x, padding_mask):
+        """
+        Parameters:
+        - x (torch.Tensor): A (batch_size, num_input_channels, input_length) tensor containing the input sEEG data.
+        - padding_mask (torch.Tensor): A (batch_size, input_length) boolean tensor containing the mask for the padding.
+        True indicates a padding position and False indicates a valid data position.
+        Returns:
+        - x (torch.Tensor): A (batch_size, num_output_channels) tensor containing the sEEG embedding.
+        """
+        x = x.permute(0, 2, 1)
+        cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        padding_mask = torch.cat((torch.zeros(x.shape[0], 1, dtype=torch.bool).to(x.device), padding_mask), dim=1)
+
+        x += self.positional_encoding
+        x = self.transformer_encoder(x, src_key_padding_mask=padding_mask)
+        x = x[:, 0, :]
+
+        x = self.proj(x)
+        return x
+
+
 # class SEEGEncoderChaFirst(SEEGEncoder):
 #     """
 #     A Transformer Encoder for sEEG data with channels matching first.
@@ -226,14 +274,22 @@ if __name__ == '__main__':
     model = SEEGEncoder(num_heads, num_encoder_layers, dim_feedforward, num_input_channels, input_length)
     print_num_params(model)
 
-    seegs = torch.randn(2, 84, 5120)
+    # seegs = torch.randn(2, 84, 5120)
+    #
+    # with torch.no_grad():
+    #     output = model(seegs)
+    # assert output.shape == (2, 768)
+    #
+    # model = SEEGEncoderCls(num_heads, num_encoder_layers, dim_feedforward, num_input_channels, input_length)
+    # print_num_params(model)
+    # with torch.no_grad():
+    #     output = model(seegs)
+    # assert output.shape == (2, 768)
 
+    model = SEEGEncoderScene(num_heads, num_encoder_layers, dim_feedforward, num_input_channels, input_length)
+    input = torch.randn(2, 84, 6865)
+    padding_mask = torch.zeros(2, 6865, dtype=torch.bool)
+    padding_mask[:, 5120:] = True
     with torch.no_grad():
-        output = model(seegs)
-    assert output.shape == (2, 768)
-
-    model = SEEGEncoderCls(num_heads, num_encoder_layers, dim_feedforward, num_input_channels, input_length)
-    print_num_params(model)
-    with torch.no_grad():
-        output = model(seegs)
+        output = model(input, padding_mask)
     assert output.shape == (2, 768)
--- model/videoencoder.py
+++ model/videoencoder.py
@@ -32,7 +32,7 @@ class VideoEncoderDino(nn.Module):
         return x
 
 
-class VideoEncoderDinoCls(nn.Module):
+class VideoEncoderDinoScene(nn.Module):
     """
     Video Encoder for the DINO features that handles variable sequence length
     """
@@ -40,7 +40,7 @@ class VideoEncoderDinoCls(nn.Module):
         super().__init__()
         max_length = 201
 
-        positional_encoding = gen_pos_encoding(max_length, 768)
+        positional_encoding = gen_pos_encoding(max_length + 1, 768)
         self.register_buffer('positional_encoding', positional_encoding)
 
         encoder_layer = nn.TransformerEncoderLayer(d_model=768, nhead=6, dim_feedforward=1024, batch_first=True)
@@ -61,6 +61,7 @@ class VideoEncoderDinoCls(nn.Module):
         """
         cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
+        padding_mask = torch.cat((torch.zeros(x.shape[0], 1, dtype=torch.bool).to(x.device), padding_mask), dim=1)
 
         x += self.positional_encoding
         x = self.transformer_encoder(x, src_key_padding_mask=padding_mask)
@@ -104,14 +105,22 @@ class VideoEncoderDinoCls(nn.Module):
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = VideoEncoderDino().to(device)
-    inputs = torch.rand(10, 150, 768).to(device)
-    with torch.no_grad():
-        outputs = model(inputs)
-    assert outputs.shape == (10, 768)
+    # model = VideoEncoderDino().to(device)
+    # inputs = torch.rand(10, 150, 768).to(device)
+    # with torch.no_grad():
+    #     outputs = model(inputs)
+    # assert outputs.shape == (10, 768)
+    #
+    # model = VideoEncoderVdFt().to(device)
+    # inputs = torch.rand(10, 768).to(device)
+    # with torch.no_grad():
+    #     outputs = model(inputs)
+    # assert outputs.shape == (10, 768)
 
-    model = VideoEncoderVdFt().to(device)
-    inputs = torch.rand(10, 768).to(device)
+    model = VideoEncoderDinoScene().to(device)
+    inputs = torch.rand(10, 201, 768).to(device)
+    padding_mask = torch.zeros(10, 201).to(device)
+    padding_mask[:, 100:] = 1
     with torch.no_grad():
-        outputs = model(inputs)
+        outputs = model(inputs, padding_mask)
     assert outputs.shape == (10, 768)
